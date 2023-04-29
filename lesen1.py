7539562075a0bba4a70d970975c3e6c7528e1560
progkhalifah <progkhalifahalsharabi@gmail.com>
2023-04-29 18:31:07 +0300

    firts publisch

---
+++ lesen1.py
@@ -0,0 +1,16 @@
+
+
+import cv2
+
+
+PH1 = cv2.imread('basic.jpg')
+PH2 = cv2.imread('images/three.jpg')
+
+if PH1.shape == PH2.shape:
+    print('The image have same size and channels')
+    diff = cv2.subtract(PH1,PH2)
+    b, g, r = cv2.split(diff)
+    if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) and cv2.countNonZero(r):
+        print('The images are completely Equel')
+else:
+    print('The iamges are not Equel')
